5f284780ff70bc2e3341902e50e99bc3e649cea9
Sam Thomas <samuel.mmthomas@gmail.com>
2024-02-09 02:08:43 +0000

    feat: Add sliding animation to navbar title

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -12,15 +12,22 @@ const navItems = [
 const Navbar = () => {
     return (
         <div className='flex flex-wrap items-center justify-between mx-auto px-6 sm:px-24 py-6'>
-            <Link href={"/"}>
-                <Image
-                    src="/images/Icon.svg"
-                    alt="logo"
-                    width={60}
-                    height={60}
-                    className="transform transition-transform duration-500 hover:rotate-90"
-                />
-            </Link>
+            <div className="group flex items-center space-x-4">
+                <Link href={"/"}>
+                    <Image
+                        src="/images/Icon.svg"
+                        alt="logo"
+                        width={60}
+                        height={60}
+                        className="transform transition-transform duration-700 group-hover:rotate-90"
+                    />
+                </Link>
+                <div className="overflow-hidden max-w-0 group-hover:max-w-full transition-all duration-700">
+                    <span className="text-2xl font-sans font-bold whitespace-nowrap">
+                        SAM THOMAS
+                    </span>
+                </div>
+            </div>
             <div className='menu hidden md:block w-auto' id='navbar'>
                 <ul className='flex p-4 md:p-0 md:flex-row md:space-x-8'>
                     {
